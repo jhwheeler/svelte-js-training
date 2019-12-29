57e512654cf0861a18db3ae63317b2477238e2a5
Jackson Holiday Wheeler <jhwheeler@gmail.com>
2019-12-29 22:37:17 +0700

    feat: async declaration examples

---
+++ src/routes/examples/async-declarations/callbacks.svelte
@@ -0,0 +1,40 @@
+<script>
+  import fetch from 'isomorphic-unfetch'
+
+  const base = 'http://numbersapi.com'
+
+  let result
+  let year
+
+  $: if (year) {
+    fetch(`${base}/${year}/year`)
+      .then(response => response.text())
+      .then(data => result = data)
+  }
+
+</script>
+
+<h1>Get a random fact for any year since 1 AD</h1>
+
+<input bind:value={year} type="number" />
+
+{#if result}
+  <h3>{result}</h3>
+{/if}
+
+<style>
+  h3 {
+    margin-top: 1rem;
+  }
+
+  input {
+    text-align: center;
+    height: 2rem;
+    width: 4rem;
+    border: 1px solid gray;
+    border-radius: 5px;
+    background-color: #f2f2f2;
+    font-size: 1rem;
+  }
+</style>
+
