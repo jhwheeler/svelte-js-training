9e027f35e99adddda6fa2557977e4b76039ff565
Jackson Holiday Wheeler <jhwheeler@gmail.com>
2019-12-30 00:19:18 +0700

    feat: markdown blog

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,27 @@
-import resolve from '@rollup/plugin-node-resolve';
-import replace from '@rollup/plugin-replace';
-import commonjs from '@rollup/plugin-commonjs';
-import svelte from 'rollup-plugin-svelte';
-import babel from 'rollup-plugin-babel';
-import { terser } from 'rollup-plugin-terser';
-import config from 'sapper/config/rollup.js';
-import pkg from './package.json';
+import resolve from 'rollup-plugin-node-resolve'
+import replace from 'rollup-plugin-replace'
+import commonjs from 'rollup-plugin-commonjs'
+import svelte from 'rollup-plugin-svelte'
+import babel from 'rollup-plugin-babel'
+import { terser } from 'rollup-plugin-terser'
+import config from 'sapper/config/rollup.js'
+import marked from 'marked'
+import pkg from './package.json'
 
-const mode = process.env.NODE_ENV;
-const dev = mode === 'development';
-const legacy = !!process.env.SAPPER_LEGACY_BUILD;
+const mode = process.env.NODE_ENV
+const dev = mode === 'development'
+const legacy = !!process.env.SAPPER_LEGACY_BUILD
 
-const onwarn = (warning, onwarn) => (warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) || onwarn(warning);
-const dedupe = importee => importee === 'svelte' || importee.startsWith('svelte/');
+const onwarn = (warning, onwarn) => (warning.code === 'CIRCULAR_DEPENDENCY' && warning.message.includes('/@sapper/')) || onwarn(warning)
+const markdown = () => ({
+	transform (md, id) {
+		if (!/\.md$/.test(id)) return null
+		const data = marked(md)
+		return {
+			code: `export default ${JSON.stringify(data.toString())}`
+		}
+	}
+})
 
 export default {
 	client: {
@@ -28,10 +37,7 @@ export default {
 				hydratable: true,
 				emitCss: true
 			}),
-			resolve({
-				browser: true,
-				dedupe
-			}),
+			resolve(),
 			commonjs(),
 
 			legacy && babel({
@@ -71,10 +77,9 @@ export default {
 				generate: 'ssr',
 				dev
 			}),
-			resolve({
-				dedupe
-			}),
-			commonjs()
+			resolve(),
+			commonjs(),
+			markdown()
 		],
 		external: Object.keys(pkg.dependencies).concat(
 			require('module').builtinModules || Object.keys(process.binding('natives'))
@@ -98,4 +103,4 @@ export default {
 
 		onwarn,
 	}
-};
+}
