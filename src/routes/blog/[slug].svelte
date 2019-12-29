7c645a32e185b431df30715eb94807fec08bb1ce
Jackson Holiday Wheeler <jhwheeler@gmail.com>
2019-12-29 22:36:46 +0700

    chore: formatting

--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -1,31 +1,31 @@
 <script context="module">
 	export async function preload({ params, query }) {
-		// the `slug` parameter is available because
-		// this file is called [slug].svelte
-		const res = await this.fetch(`blog/${params.slug}.json`);
-		const data = await res.json();
+		const res = await this.fetch(`blog/${params.slug}.json`)
+		const data = await res.json()
 
 		if (res.status === 200) {
-			return { post: data };
+			return { post: data }
 		} else {
-			this.error(res.status, data.message);
+			this.error(res.status, data.message)
 		}
 	}
 </script>
 
 <script>
-	export let post;
+	export let post
 </script>
 
+<svelte:head>
+	<title>{post.title}</title>
+</svelte:head>
+
+<h1>{post.title}</h1>
+
+<div class='content'>
+	{@html post.html}
+</div>
+
 <style>
-	/*
-		By default, CSS is locally scoped to the component,
-		and any unused styles are dead-code-eliminated.
-		In this page, Svelte can't know which elements are
-		going to appear inside the {{{post.html}}} block,
-		so we have to use the :global(...) modifier to target
-		all elements inside .content
-	*/
 	.content :global(h2) {
 		font-size: 1.4em;
 		font-weight: 500;
@@ -52,13 +52,3 @@
 		margin: 0 0 0.5em 0;
 	}
 </style>
-
-<svelte:head>
-	<title>{post.title}</title>
-</svelte:head>
-
-<h1>{post.title}</h1>
-
-<div class='content'>
-	{@html post.html}
-</div>
--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -10,13 +10,6 @@
 	export let posts;
 </script>
 
-<style>
-	ul {
-		margin: 0 0 1em 0;
-		line-height: 1.5;
-	}
-</style>
-
 <svelte:head>
 	<title>Blog</title>
 </svelte:head>
@@ -25,10 +18,13 @@
 
 <ul>
 	{#each posts as post}
-		<!-- we're using the non-standard `rel=prefetch` attribute to
-				tell Sapper to load the data for the page as soon as
-				the user hovers over the link or taps it, instead of
-				waiting for the 'click' event -->
 		<li><a rel='prefetch' href='blog/{post.slug}'>{post.title}</a></li>
 	{/each}
 </ul>
+
+<style>
+	ul {
+		margin: 0 0 1em 0;
+		line-height: 1.5;
+	}
+</style>
